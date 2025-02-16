b658a89dac5ae1df4d4a9b328af88cd486eeb294
Michael DiGiacomo <mrdigi2025@gmail.com>
2025-02-15 21:39:14 -0500

    added ai if it moves first, and added an edit board function to make that easier

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 board = [
     ["-", "-", "-"],
     ["-", "-", "-"],
@@ -12,9 +14,21 @@ def print_board():
         print()
     print()
 
+def edit_board(coord, piece):
+    board[coord[0]][coord[1]] = piece
+
 def decide_next_move():
     player = "X"
     computer = "O"
     blank = "-"
 
+    total_moves = len([column for row in board for column in row if not column == blank])
+    player_move_locations = [(row, column) for row in range(len(board)) for column in range(len(board[row])) if board[row][column] == player]
+    computer_move_locations = [(row, column) for row in range(len(board)) for column in range(len(board[row])) if board[row][column] == computer]
+    
+    if total_moves == 0:
+        new_move = (random.choice([0, 2]), random.choice([0, 2]))
+        edit_board(new_move, computer)
+
+decide_next_move()
 print_board()
