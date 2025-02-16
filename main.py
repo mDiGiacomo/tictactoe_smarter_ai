72dd73a1aca2b3591a59d229ba8e379429de554c
Michael DiGiacomo <mrdigi2025@gmail.com>
2025-02-15 21:13:21 -0500

    added the board and its ability to be printed

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello, World!")
+board = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
+]
+
+def print_board(board):
+    print()
+    for row in board:
+        for column in row:
+            print(f" {column}", end="")
+        print()
+    print()
+    
+print_board(board)
