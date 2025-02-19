cc12a3f6ccb9bf9faf85282f8029785109d75aaf
Michael DiGiacomo <mrdigi2025@gmail.com>
2025-02-19 12:01:17 -0500

    transfered everything to a class, next im going to rename computer to player 2 since this also needs to be a pvp or pvc.

--- main.py
+++ main.py
@@ -1,74 +1,78 @@
 import random
 
-player = "X"
-computer = "O"
-blank = "-"
-board = [
-    ["-", "-", "-"],
-    ["-", "-", "-"],
-    ["-", "-", "-"]
-]
+class Game:
+    def __init__(self, player = "X", computer="O", blank="-"):
+        self.player = "X"
+        self.computer = "O"
+        blank = "-"
+        board = [
+            ["-", "-", "-"],
+            ["-", "-", "-"],
+            ["-", "-", "-"]
+        ]
 
-def print_board():
-    print()
-    for row in board:
-        for column in row:
-            print(f" {column}", end="")
+    def print_board(self):
         print()
-    print()
+        for row in self.board:
+            for column in row:
+                print(f" {column}", end="")
+            print()
+        print()
 
-def edit_board(coord, piece):
-    board[coord[0]][coord[1]] = piece
+    def edit_board(self, coord, piece):
+        self.board[coord[0]][coord[1]] = piece
 
-def check_for_win(board=board):
-    for user in [player, computer]:
-        rows = [row for row in board if len(set(row)) == 1 and set(row) == {user}]
-        columns = [row for row in [list(x) for x in zip(*board)] if len(set(row)) == 1 and set(row) == {user}]
-        left_diagonal = len(set([item for sublist in board for item in sublist][::4])) and set([item for sublist in board for item in sublist][::4]) == {user}
-        right_diagonal = len(set([item for sublist in board for item in sublist][2::2][:3])) and set([item for sublist in board for item in sublist][2::2][:3]) == {user}
-        
-        #print(f"{rows}\n{columns}\n{left_diagonal}\n{right_diagonal}")
+    def check_for_win(self, board=None):
+        if board == None:
+            board = self.board
 
-        if rows or columns or left_diagonal or right_diagonal:
-            return user
+        for user in [self.player, self.computer]:
+            rows = [row for row in board if len(set(row)) == 1 and set(row) == {user}]
+            columns = [row for row in [list(x) for x in zip(*board)] if len(set(row)) == 1 and set(row) == {user}]
+            left_diagonal = len(set([item for sublist in board for item in sublist][::4])) and set([item for sublist in board for item in sublist][::4]) == {user}
+            right_diagonal = len(set([item for sublist in board for item in sublist][2::2][:3])) and set([item for sublist in board for item in sublist][2::2][:3]) == {user}
 
-    return False
+            if rows or columns or left_diagonal or right_diagonal:
+                return user
+        return False
 
-def check_possible_wins(user):
-    for row_index, row in enumerate(board):
-        for column_index, column in enumerate(row):
-            if column != blank:
-                continue
-            
-            possible_board = [row[:] for row in board]
-            possible_board[row_index][column_index] = user
-            if check_for_win(possible_board) == user:
-                return [row_index, column_index]
-    
-    return False
+    def check_possible_wins(self, user):
+        for row_index, row in enumerate(self.board):
+            for column_index, column in enumerate(row):
+                if column != blank:
+                    continue
+                
+                possible_board = [row[:] for row in self.board]
+                possible_board[row_index][column_index] = user
+                if check_for_win(possible_board) == user:
+                    return [row_index, column_index]
+        return False
 
 def decide_next_move():
     corners = [[0, 0], [0, 2], [2, 0], [2, 2]]
     edges = [[0, 1], [1, 0], [1, 2], [2, 1]]
     center = [1, 1]
 
-    total_moves = len([column for row in board for column in row if not column == blank])
-    player_locations = [[row, column] for row in range(len(board)) for column in range(len(board[row])) if board[row][column] == player]
-    computer_locations = [[row, column] for row in range(len(board)) for column in range(len(board[row])) if board[row][column] == computer]
+    total_moves = len([column for row in self.board for column in row if not column == self.blank])
+    player_locations = [[row, column] for row in range(len(self.board)) for column in range(len(self.board[row])) if self.board[row][column] == self.player]
+    computer_locations = [[row, column] for row in range(len(self.board)) for column in range(len(self.board[row])) if self.board[row][column] == self.computer]
+    blank_locations = [[row, column] for row in range(len(self.board)) for column in range(len(self.board[row])) if self.board[row][column] == self.blank]
     
-    player_possible_win = check_possible_wins(player)
-    computer_possible_win = check_possible_wins(computer)
+    player_possible_win = check_possible_wins(self.player)
+    computer_possible_win = check_possible_wins(self.computer)
 
     # make a valid random move to have a default move ready
     while True:
-        coord = [random.randint(0, 2), random.randint(0, 2)]
-        if board[coord[0]][coord[1]] == '-':
+        coord = random.choice(blank_locations)
+        if self.board[coord[0]][coord[1]] == self.blank:
             new_move = coord
             break
-
+    
+    # if the computer gets the first move put the piece in the corners
     if total_moves == 0:
-        new_move = (random.choice([0, 2]), random.choice([0, 2]))
-        edit_board(new_move, computer)
+        new_move = random.choice(corners)
+
+    # if the player gets the first move
     elif total_moves == 1:
         player_location = player_locations[0]
         if player_location == center:
@@ -77,12 +81,15 @@ def decide_next_move():
             new_move = center
         else:
             new_move = [2 if coord == 0 else random.choice([0, 2]) if coord == 1 else 0 for coord in player_location]
-            print(new_move)
+    
+    # if the computer and player have each made a move
     elif total_moves == 2:
         if (player_locations[0] in corners) or (player_locations[0] in edges):
             new_move = center
         else: # if the player moved to the center play the reciprocal of the current computer move
             new_move = [2 if coord == 0 else random.choice([0, 2]) if coord == 1 else 0 for coord in computer_locations[0]]
+    
+    # if the player has 2 moves and the computer has only made one move
     elif total_moves == 3:
         # if the player has both in the corner
         if (player_locations[0] in corners) and (player_locations[1] in corners):
@@ -103,7 +110,4 @@ def decide_next_move():
     elif player_possible_win:
         new_move = player_possible_win
 
-    edit_board(new_move, computer)
-
-decide_next_move()
-print_board()
+    edit_board(new_move, self.computer)
